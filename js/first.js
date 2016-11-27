0881415d8d1d0cb4aac2acaea742e3f0abd3e757
Radek Knapkiewicz <radknap@gmail.com>
2016-11-27 21:37:12 +0100

    commit-3

--- js/first.js
+++ js/first.js
@@ -1,11 +1,32 @@
 $( document ).ready(function() {
-	/*$('.carousel').carousel({
+	
+    $('.carousel').carousel({
 	    interval: 4000,
-	});*/
+	});
+    
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
 
 	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
     	event.preventDefault();
     	$(this).ekkoLightbox();
 	});
+    
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
 
 });
