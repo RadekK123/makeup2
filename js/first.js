646b0121967eef98c69ab0e65d500731db74241a
Radek Knapkiewicz <radknap@gmail.com>
2016-11-27 19:17:54 +0100

    commit-2

--- js/first.js
+++ js/first.js
@@ -1,17 +1,11 @@
 $( document ).ready(function() {
-	$('.carousel').carousel({
-    interval: 4000,
-    });
+	/*$('.carousel').carousel({
+	    interval: 4000,
+	});*/
 
-    $('#myTabs a').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    	event.preventDefault();
+    	$(this).ekkoLightbox();
+	});
 
-    $(document).on('click', '[data-toggle="lightbox"]',             function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
 });
-
-
